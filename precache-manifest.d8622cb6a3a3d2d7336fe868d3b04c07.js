8bb26628ce6c392c0887999f236b2c43cb3575a1
jtbspace <jtbspace.tech@gmail.comm>
2020-03-22 17:03:00 +1300

    Updates

---
+++ precache-manifest.d8622cb6a3a3d2d7336fe868d3b04c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76766f4c2ee12d1b01d98cacc4f55921",
+    "url": "/jtbspace/google_search.git/index.html"
+  },
+  {
+    "revision": "d7854c1fa40bfba82719",
+    "url": "/jtbspace/google_search.git/static/js/2.86fb1db9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jtbspace/google_search.git/static/js/2.86fb1db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f06c5978b7c3a236314",
+    "url": "/jtbspace/google_search.git/static/js/main.a00e54e3.chunk.js"
+  },
+  {
+    "revision": "fd55cf7564f7c6781746",
+    "url": "/jtbspace/google_search.git/static/js/runtime-main.79507ca9.js"
+  }
+]);
